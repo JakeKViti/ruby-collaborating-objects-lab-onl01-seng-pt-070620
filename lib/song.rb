22d31f6c783084ee2ce0d188de51776c3606e3a8
Jacob Viti <thevitij64@gmail.com>
2020-08-03 06:06:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,13 @@
 class Song
   attr_accessor :name, :artist, :title
 
-  @@all = []
-
   def initialize(name)
     @name = name
-    @@all << self
   end
 
-  def self.all
-    @@all
-  end
+  #def self.all
+  #  @@all
+#  end
 
   def self.new_by_filename(filename)
      song = self.new
