f89c2738885bf076d344f26797b160a9de49d5a1
Jacob Viti <thevitij64@gmail.com>
2020-08-03 05:57:56 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,8 +10,8 @@ class MP3Importer
   end
 
   def import(list_of_filenames)
-    list_of_filenames.each do{ |filename| Song.new_by_filename(filename) }
-    end
+    list_of_filenames.each { |filename| Song.new_by_filename(filename) }
+
   end
 
 end
