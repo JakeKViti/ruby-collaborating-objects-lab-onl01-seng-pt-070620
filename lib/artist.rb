610743a4c703a6aa18dc45b0f0c6b8789385e6a2
Jacob Viti <thevitij64@gmail.com>
2020-08-03 04:47:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ class Artist
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def self.all
@@ -11,7 +12,7 @@ class Artist
   end
 
   def add_song
-
+     @songs << song
   end
 
   def songs
