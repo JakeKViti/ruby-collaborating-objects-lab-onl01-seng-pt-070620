7b46f4b8eef7c1bfe76ba31c7a6f0f250422765a
Jacob Viti <thevitij64@gmail.com>
2020-08-03 17:38:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     artistName = filename.split(" - ")[0]
     songName = filename.split(" - ")[1]
     song = Song.new(songName)
-    song.artistName = artistName
+    song.artist_name = artistName
     song
    end
 
