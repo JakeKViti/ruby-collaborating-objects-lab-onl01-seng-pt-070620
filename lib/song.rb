f2787bf1295e124ec2155aface0b72d5ef11d4a5
Jacob Viti <thevitij64@gmail.com>
2020-08-03 18:47:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,8 +24,9 @@ class Song
 
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self.artist)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
     #binding.pry
   end
 end
